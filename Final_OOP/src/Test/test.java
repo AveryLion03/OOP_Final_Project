41371765fb59fd373065998aa6bd43e48bcd5b4c
AveryLion03 <95372129+AveryLion03@users.noreply.github.com>
2024-05-14 17:08:42 +0200

    Hello!

--- Final_OOP/src/Test/test.java
+++ Final_OOP/src/Test/test.java
@@ -3,7 +3,7 @@ package Test;
 public class test {
 	//hi
 	public static void main (String [] args) {
-		
+		System.out.print("Hello, its me. Ive been calling about your cars extended warranty");
 	}
 
 }
