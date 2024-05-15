853c89420b7a6a16a8785bb6b1e1052dccc86dc9
AveryLion03 <95372129+AveryLion03@users.noreply.github.com>
2024-05-15 09:24:14 +0200

    read terminal

--- src/Code/myFoodora.java
+++ src/Code/myFoodora.java
@@ -1,7 +1,25 @@
 package Code;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class myFoodora {
-	public static void main (String [] args) {
-		System.out.println("Ello Govner");
+	public static void main (String [] args) throws IOException {
+		boolean loop = true;
+		// Enter data using BufferReader
+		while (loop) {
+	        BufferedReader reader = new BufferedReader(
+	            new InputStreamReader(System.in));
+	
+	        // Reading data using readLine
+	        String name = reader.readLine();
+	        if(name.equals("Exit")||name.equals("exit")){
+	        	loop = false;
+	        }
+	        else {
+	        	// Printing the read line
+	        	System.out.println(name);
+	        }
+		}
 	}
 }
